e2e1beb1be1b5b493240cc7ab9dc9d67346b1a0d
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-10 16:58:20 +0600

    injected 'UserManager', and 'SignInManager' services via constructor to 'AccountController'

--- FindMusic/Controllers/AccountController.cs
+++ FindMusic/Controllers/AccountController.cs
@@ -1,3 +1,5 @@
+using FindMusic.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FindMusic.Controllers
@@ -5,6 +7,15 @@ namespace FindMusic.Controllers
     [Route("Account/[action]/{username?}")]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return RedirectToAction("Index", "Home");
