63c4608c30d66934fecae8a73d16a702eefa933c
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-11 16:24:00 +0600

    injected 'UserManager' and 'SignInManager' services via constructor injection, thus producing two private fields, in 'AdministrationController' class

--- FindMusic/Controllers/AdministrationController.cs
+++ FindMusic/Controllers/AdministrationController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FindMusic.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FindMusic.Controllers
 {
 	public class AdministrationController : Controller
 	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public AdministrationController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+		{
+			_userManager = userManager;
+			_roleManager = roleManager;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
