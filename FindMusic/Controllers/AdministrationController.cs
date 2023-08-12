94b01612b9166f6b386482f543bb22b7492dfae7
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-12 14:40:04 +0600

    added a HttpGet action method named 'EditUser' to 'AdministrationController' class

--- FindMusic/Controllers/AdministrationController.cs
+++ FindMusic/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
 ﻿using FindMusic.Models;
+using FindMusic.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,6 +26,37 @@ namespace FindMusic.Controllers
 		{
 			var users = _userManager.Users;
 			return View(users);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> EditUser(string id)
+		{
+			var user = await _userManager.FindByIdAsync(id);
+
+			if (user == null)
+			{
+				Response.StatusCode = 404;
+				ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+				return View("NotFound");
+			}
+
+			var userClaims = await _userManager.GetClaimsAsync(user);
+			var userRoles = await _userManager.GetRolesAsync(user);
+
+			var model = new EditUserViewModel
+			{
+				Id = user.Id,
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				Email = user.Email,
+				UserName = user.UserName,
+				Gender = user.Gender,
+				DOB = user.DOB,
+				Claims = userClaims.Select(c => c.Value).ToList(),
+				Roles = userRoles
+			};
+
+			return View(model);
 		}
 	}
 }
