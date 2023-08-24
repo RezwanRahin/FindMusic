04e96e90b49ce1e7fd9761183ba76bf2c8e9e97e
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-24 16:53:00 +0600

    created a repository class, implementing its interface, named SeriesRepository

---
+++ FindMusic/Repository/SeriesRepository.cs
@@ -0,0 +1,47 @@
+﻿using FindMusic.Models;
+
+namespace FindMusic.Repository
+{
+	public class SeriesRepository : ISeriesRepository
+	{
+		public Task<Series> Add(Series series)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Series> Delete(Series series)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<Series>> GetAllSeries()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<Series>> GetLatestSeries()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Series> GetSeriesById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Series> GetSeriesBySlug(string slug)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Series> GetSeriesBySlugWithRelatedData(string slug)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Series> Update(Series modifiedSeries)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
