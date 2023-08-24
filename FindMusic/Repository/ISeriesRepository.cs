2bd5dd332e3ce52e21a7eb992e4d09968bd16a19
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-24 16:51:08 +0600

    created a repository interface named 'ISeriesRepository'

---
+++ FindMusic/Repository/ISeriesRepository.cs
@@ -0,0 +1,16 @@
+﻿using FindMusic.Models;
+
+namespace FindMusic.Repository
+{
+	public interface ISeriesRepository
+	{
+		Task<Series> Add(Series series);
+		Task<Series?> GetSeriesById(int id);
+		Task<Series?> GetSeriesBySlug(string slug);
+		Task<Series?> GetSeriesBySlugWithRelatedData(string slug);
+		Task<IEnumerable<Series>> GetAllSeries();
+		Task<IEnumerable<Series>> GetLatestSeries();
+		Task<Series> Update(Series modifiedSeries);
+		Task<Series> Delete(Series series);
+	}
+}
