4cc9285e44aec7728e7720533d5c95b435c3a702
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-25 13:44:38 +0600

    created a repository class, implementing its interface, named SeasonRepository

---
+++ FindMusic/Repository/SeasonRepository.cs
@@ -0,0 +1,37 @@
+﻿using FindMusic.Models;
+
+namespace FindMusic.Repository
+{
+    public class SeasonRepository : ISeasonRepository
+    {
+        public Task<Season> Add(Season season)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Season> Delete(Season season)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Season?> GetSeason(int number, string seriesSlug)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Season?> GetSeasonWithRelatedData(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Season?> GetSeasonWithRelatedData(int number, string seriesSlug)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Season> Update(Season season)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
