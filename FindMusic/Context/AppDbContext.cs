fc640251418c434e3782eeb8bf41a310da807e1a
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-25 21:18:34 +0600

    added fields to 'AppDbContext' class

--- FindMusic/Context/AppDbContext.cs
+++ FindMusic/Context/AppDbContext.cs
@@ -9,5 +9,15 @@ namespace FindMusic.Context
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Series> Series { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<Timestamp> Timestamps { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+        public DbSet<Donation> Donations { get; set; }
     }
 }
