9def539c8cac3c2e633d98dc78ef1e099c204bce
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-25 16:21:44 +0600

    added a constructor, injecting 'ISeriesRepository', 'UserManager', and 'IWebHostEnvironment' services to 'SeriesController' class

--- FindMusic/Controllers/SeriesController.cs
+++ FindMusic/Controllers/SeriesController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FindMusic.Models;
+using FindMusic.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FindMusic.Controllers
 {
 	public class SeriesController : Controller
 	{
+		private readonly ISeriesRepository _seriesRepository;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly IWebHostEnvironment _hostEnvironment;
+
+		public SeriesController(ISeriesRepository seriesRepository, UserManager<ApplicationUser> userManager,
+			IWebHostEnvironment hostEnvironment)
+		{
+			_seriesRepository = seriesRepository;
+			_userManager = userManager;
+			_hostEnvironment = hostEnvironment;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
