71059bbdf2405d84bda3a5518de2dd15602c3ea5
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-25 13:43:50 +0600

    created a repository interface named 'ISeasonRepository'

---
+++ FindMusic/Repository/ISeasonRepository.cs
@@ -0,0 +1,14 @@
+﻿using FindMusic.Models;
+
+namespace FindMusic.Repository
+{
+    public interface ISeasonRepository
+    {
+        Task<Season> Add(Season season);
+        Task<Season?> GetSeason(int number, string seriesSlug);
+        Task<Season?> GetSeasonWithRelatedData(int id);
+        Task<Season?> GetSeasonWithRelatedData(int number, string seriesSlug);
+        Task<Season> Update(Season season);
+        Task<Season> Delete(Season season);
+    }
+}
