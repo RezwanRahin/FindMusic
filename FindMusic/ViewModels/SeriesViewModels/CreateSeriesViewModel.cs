edf4bc17235185bf48ca5dd4b1a35f7510cc2d0d
rezwan <rezwanshahriorrahin@gmail.com>
2023-08-30 21:35:14 +0600

    created a ViewModel named 'CreateSeriesViewModel'

---
+++ FindMusic/ViewModels/SeriesViewModels/CreateSeriesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FindMusic.ViewModels.SeriesViewModels
+{
+	public class CreateSeriesViewModel
+	{
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public IFormFile Photo { get; set; }
+	}
+}
