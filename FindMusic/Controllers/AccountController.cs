7b5cceefee8def9ffbd7c219b34bfe47f8317f91
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-01 13:37:04 +0600

    added route attribute to 'AccountController' class

--- FindMusic/Controllers/AccountController.cs
+++ FindMusic/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FindMusic.Controllers
 {
+    [Route("Account/[action]/{username?}")]
     public class AccountController : Controller
     {
         public IActionResult Index()
