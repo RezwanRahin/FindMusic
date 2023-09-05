ac33d86bee85fd1f9f68c0cf6a365b46f39dbf85
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-05 16:08:39 +0600

    created a ViewModel named 'UpdateSeriesViewModel'

---
+++ FindMusic/ViewModels/SeriesViewModels/UpdateSeriesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FindMusic.ViewModels.SeriesViewModels
+{
+	public class UpdateSeriesViewModel : SeriesDetailsViewModel
+	{
+		public string? Slug { get; set; }
+		public string? PhotoPath { get; set; }
+		public IFormFile? Photo { get; set; }
+	}
+}
