5f01e1f9282a094afb50410932a9599be4103b2d
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-08 13:23:08 +0600

    added a constructor, injecting 'IMovieRepository', 'UserManager', and 'IWebHostEnvironment' services to 'MovieController' class

--- FindMusic/Controllers/MovieController.cs
+++ FindMusic/Controllers/MovieController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FindMusic.Models;
+using FindMusic.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FindMusic.Controllers
 {
     public class MovieController : Controller
     {
+        private readonly IMovieRepository _movieRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public MovieController(IMovieRepository movieRepository, UserManager<ApplicationUser> userManager,
+            IWebHostEnvironment hostEnvironment)
+        {
+            _movieRepository = movieRepository;
+            _userManager = userManager;
+            _hostEnvironment = hostEnvironment;
+        }
+
         public IActionResult Index()
         {
             return View();
