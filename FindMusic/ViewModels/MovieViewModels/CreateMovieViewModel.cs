cd3c003373e0cf5f82f4af6e45e8d62773789087
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-12 00:36:35 +0600

    created a ViewModel named 'CreateMovieViewModel'

---
+++ FindMusic/ViewModels/MovieViewModels/CreateMovieViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FindMusic.ViewModels.MovieViewModels
+{
+    public class CreateMovieViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public IFormFile Photo { get; set; }
+    }
+}
