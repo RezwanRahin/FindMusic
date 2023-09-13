0d679183863c421cbc6c0a0a202b0ee14266f556
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-13 12:58:25 +0600

    created a HttpGet action method named 'Create' to 'MovieController' class

--- FindMusic/Controllers/MovieController.cs
+++ FindMusic/Controllers/MovieController.cs
@@ -29,5 +29,11 @@ namespace FindMusic.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
