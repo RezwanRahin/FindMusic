205e6a5552a1e2eeab1b4e513caa61461f27d38a
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-17 00:20:54 +0600

    created a HttpGet action method named 'Details' to 'MovieController' class

--- FindMusic/Controllers/MovieController.cs
+++ FindMusic/Controllers/MovieController.cs
@@ -61,5 +61,32 @@ namespace FindMusic.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("[controller]/[action]/{slug}")]
+        public async Task<IActionResult> Details(string slug)
+        {
+            var movie = await _movieRepository.GetMovieWithRelatedData(slug);
+
+            if (movie == null)
+            {
+                Response.StatusCode = 404;
+                ViewBag.ErrorMessage = $"Movie like {slug} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new MovieDetailsViewModel
+            {
+                Id = movie.Id,
+                Name = movie.Name,
+                Slug = movie.Slug,
+                ReleaseDate = movie.ReleaseDate,
+                Poster = new PosterViewModel(movie.PhotoPath),
+                Contributor = new ContributorViewModel(movie.User),
+            };
+
+            return View(model);
+        }
     }
 }
