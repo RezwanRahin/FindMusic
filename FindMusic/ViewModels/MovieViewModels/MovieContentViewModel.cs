df8b69c0bc15b93d8e3fa9841e7ae9ec68c6a379
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-17 22:36:18 +0600

    created a ViewModel named 'MovieContentViewModel'

---
+++ FindMusic/ViewModels/MovieViewModels/MovieContentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FindMusic.ViewModels.MovieViewModels
+{
+    public class MovieContentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public ContributorViewModel? Contributor { get; set; }
+        public PosterViewModel Poster { get; set; }
+    }
+}
