34fdb9d1ae896311069f3416dc94746ada7c31a2
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-17 14:50:09 +0600

    created a ViewModel named 'UpdateMovieViewModel'

---
+++ FindMusic/ViewModels/MovieViewModels/UpdateMovieViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FindMusic.ViewModels.MovieViewModels
+{
+    public class UpdateMovieViewModel : MovieContentViewModel
+    {
+        public string? Slug { get; set; }
+        public string? PhotoPath { get; set; }
+        public IFormFile? Photo { get; set; }
+    }
+}
