ac6a05f3f43e0404ae6ed009f8faba7c95c9f9a5
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-22 12:38:34 +0600

    created a repository interface named 'IMovieRepository'

---
+++ FindMusic/Repository/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using FindMusic.Models;
+
+namespace FindMusic.Repository
+{
+	public interface IMovieRepository
+	{
+		Task<Movie> Add(Movie movie);
+		Task<Movie?> GetMovieById(int id);
+		Task<Movie?> GetMovieBySlug(string slug);
+		Task<Movie?> GetMovieWithRelatedData(string slug);
+		Task<IEnumerable<Movie>> GetAllMovies();
+		Task<IEnumerable<Movie>> GetLatestMovies();
+		Task<Movie> Update(Movie modifiedMovie);
+		Task<Movie> Delete(Movie movie);
+	}
+}
