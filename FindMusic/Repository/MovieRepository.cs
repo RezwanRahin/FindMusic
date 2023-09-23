30a4df801a7eda32ba9b6487148950fa4d08857c
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-23 16:01:27 +0600

    created a repository class, implementing its interface, named 'MovieRepository'

---
+++ FindMusic/Repository/MovieRepository.cs
@@ -0,0 +1,47 @@
+﻿using FindMusic.Models;
+
+namespace FindMusic.Repository
+{
+	public class MovieRepository : IMovieRepository
+	{
+		public Task<Movie> Add(Movie movie)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Movie> Delete(Movie movie)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<Movie>> GetAllMovies()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<Movie>> GetLatestMovies()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Movie?> GetMovieById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Movie?> GetMovieBySlug(string slug)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Movie?> GetMovieWithRelatedData(string slug)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Movie> Update(Movie modifiedMovie)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
