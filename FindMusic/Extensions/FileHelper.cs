f9b96433ee5c1478b8aa4a9731d29d890698629e
rezwan <rezwanshahriorrahin@gmail.com>
2023-09-26 18:06:41 +0600

    added a method named 'ProcessUploadedFile' to 'FileHelper' class

--- FindMusic/Extensions/FileHelper.cs
+++ FindMusic/Extensions/FileHelper.cs
@@ -2,6 +2,18 @@
 {
 	public static class FileHelper
 	{
-		
+		public static string ProcessUploadedFile(this IFormFile photoFile, IWebHostEnvironment hostEnvironment)
+		{
+			var uploadsFolder = Path.Combine(hostEnvironment.WebRootPath, "images");
+			var uniqueFileName = Guid.NewGuid().ToString() + '_' + photoFile.FileName;
+			var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+			using (var fileStream = new FileStream(filePath, FileMode.Create))
+			{
+				photoFile.CopyTo(fileStream);
+			}
+
+			return uniqueFileName;
+		}
 	}
 }
