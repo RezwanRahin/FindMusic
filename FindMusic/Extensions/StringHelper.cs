7e862807fe2711e6f4e1e3285052a598d4c83794
rezwan <rezwanshahriorrahin@gmail.com>
2023-10-06 17:07:06 +0600

    added a method named 'RemoveAccents' to 'StringHelper' class

--- FindMusic/Extensions/StringHelper.cs
+++ FindMusic/Extensions/StringHelper.cs
@@ -1,7 +1,18 @@
-﻿namespace FindMusic.Extensions
+﻿using System.Globalization;
+using System.Text;
+
+namespace FindMusic.Extensions
 {
 	public static class StringHelper
 	{
+		public static string RemoveAccents(this string text)
+		{
+			if (string.IsNullOrWhiteSpace(text))
+				return text;
 
+			text = text.Normalize(NormalizationForm.FormD);
+			var chars = text.Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark).ToArray();
+			return new string(chars).Normalize(NormalizationForm.FormC);
+		}
 	}
 }
