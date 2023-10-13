dd18a52c79e4a4fc1a60853d4eb45f879a07d833
rezwan <rezwanshahriorrahin@gmail.com>
2023-10-13 13:32:01 +0600

    created a ViewModel named 'CardViewModel'

---
+++ FindMusic/ViewModels/CardViewModel.cs
@@ -0,0 +1,8 @@
+namespace FindMusic.ViewModels
+{
+    public class CardViewModel : PosterViewModel
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}
