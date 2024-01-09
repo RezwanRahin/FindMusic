2ad288ed7c49723c7af071c0469a1db9fb4567cd
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-09 17:20:32 +0600

    added a HttpGet action method named 'Create' to 'SeasonController' class

--- FindMusic/Controllers/SeasonController.cs
+++ FindMusic/Controllers/SeasonController.cs
@@ -1,5 +1,6 @@
 ﻿using FindMusic.Models;
 using FindMusic.Repository;
+using FindMusic.ViewModels.SeasonViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,6 +26,27 @@ namespace FindMusic.Controllers
         {
             var season = await _seasonRepository.GetSeason(number, seriesSlug);
             return season == null ? Json(true) : Json($"Season with Number = {number} already exists!");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create(string seriesSlug)
+        {
+            var series = await _seriesRepository.GetSeriesBySlug(seriesSlug);
+
+            if (series == null)
+            {
+                Response.StatusCode = 404;
+                ViewBag.ErrorMessage = $"Series like {seriesSlug} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new CreateSeasonViewModel
+            {
+                SeriesName = series.Name,
+                SeriesSlug = series.Slug,
+            };
+
+            return View(model);
         }
     }
 }
