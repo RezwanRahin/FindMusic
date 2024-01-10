4069c92073010e66a62356ade9fa08d464b4237d
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-10 18:35:56 +0600

    created a ViewModel as 'SeasonDetailsViewModel'

---
+++ FindMusic/ViewModels/SeasonViewModels/SeasonDetailsViewModel.cs
@@ -0,0 +1,7 @@
+namespace FindMusic.ViewModels.SeasonViewModels
+{
+    public class SeasonDetailsViewModel : SeasonContentViewModel
+    {
+        public ICollection<int>? Episodes { get; set; }
+    }
+}
