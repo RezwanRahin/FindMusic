02c7276eabd6c88d5dcfb0cbeba046b4bd6bd8f1
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-10 18:32:03 +0600

    created a ViewModel as 'RelatedSeriesViewModel'

---
+++ FindMusic/ViewModels/RelatedSeriesViewModel.cs
@@ -0,0 +1,10 @@
+namespace FindMusic.ViewModels
+{
+    public class RelatedSeriesViewModel
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+
+        public PosterViewModel Poster { get; set; }
+    }
+}
