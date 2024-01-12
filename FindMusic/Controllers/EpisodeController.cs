27f0fe4c8328d2a77864d76db03b4c79a653796a
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-12 16:06:25 +0600

    added a constructor, injecting 'ISeasonRepository', 'IEpisodeRepository', and 'UserManager' services, to 'EpisodeController'

--- FindMusic/Controllers/EpisodeController.cs
+++ FindMusic/Controllers/EpisodeController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FindMusic.Models;
+using FindMusic.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FindMusic.Controllers
 {
     public class EpisodeController : Controller
     {
+        private readonly ISeasonRepository _seasonRepository;
+        private readonly IEpisodeRepository _episodeRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
 
+        public EpisodeController(ISeasonRepository seasonRepository, IEpisodeRepository episodeRepository,
+            UserManager<ApplicationUser> userManager)
+        {
+            _seasonRepository = seasonRepository;
+            _episodeRepository = episodeRepository;
+            _userManager = userManager;
+        }
     }
 }
