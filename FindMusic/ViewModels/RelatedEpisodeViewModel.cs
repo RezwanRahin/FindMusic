e8810da64c257fb96f0a56223c655b747d57a21d
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-17 18:44:01 +0600

    created a ViewModel named 'RelatedEpisodeViewModel'

---
+++ FindMusic/ViewModels/RelatedEpisodeViewModel.cs
@@ -0,0 +1,10 @@
+namespace FindMusic.ViewModels
+{
+    public class RelatedEpisodeViewModel
+    {
+        public int Number { get; set; }
+        public int Season { get; set; }
+        public string SeriesName { get; set; }
+        public string SeriesSlug { get; set; }
+    }
+}
