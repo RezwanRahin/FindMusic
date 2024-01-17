0b528f042e666e4f5eee877a29a11e6cd99c7e0f
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-17 17:34:14 +0600

    added a constructor, injecting 'UserManager', 'ITimestampRepository', 'IEpisodeRepository', and 'IMovieRepository' services to 'TimestampController' class

--- FindMusic/Controllers/TimestampController.cs
+++ FindMusic/Controllers/TimestampController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FindMusic.Models;
+using FindMusic.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FindMusic.Controllers
 {
     public class TimestampController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ITimestampRepository _timestampRepository;
+        private readonly IEpisodeRepository _episodeRepository;
+        private readonly IMovieRepository _movieRepository;
 
+        public TimestampController(UserManager<ApplicationUser> userManager, ITimestampRepository timestampRepository,
+            IEpisodeRepository episodeRepository, IMovieRepository movieRepository)
+        {
+            _userManager = userManager;
+            _timestampRepository = timestampRepository;
+            _episodeRepository = episodeRepository;
+            _movieRepository = movieRepository;
+        }
     }
 }
