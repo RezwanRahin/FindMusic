b34ad7b38b8a50c3413a2c6aeebe4acf1e4f8315
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-17 19:01:26 +0600

    added a HttpGet action method named 'Create' to 'TimestampController' class

--- FindMusic/Controllers/TimestampController.cs
+++ FindMusic/Controllers/TimestampController.cs
@@ -1,5 +1,7 @@
 ﻿using FindMusic.Models;
 using FindMusic.Repository;
+using FindMusic.ViewModels;
+using FindMusic.ViewModels.TimestampViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +21,69 @@ namespace FindMusic.Controllers
             _timestampRepository = timestampRepository;
             _episodeRepository = episodeRepository;
             _movieRepository = movieRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create(int id, string type)
+        {
+            if (type == "episode")
+            {
+                var episode = await _episodeRepository.GetEpisodeWithRelatedData(id);
+
+                if (episode == null)
+                {
+                    Response.StatusCode = 404;
+                    ViewBag.ErrorMessage = $"Episode with Id = {id} cannot be found";
+                    return View("NotFound");
+                }
+
+                var model = new CreateTimestampViewModel
+                {
+                    Content = new ContentDetailsViewModel
+                    {
+                        Episode = new RelatedEpisodeViewModel
+                        {
+                            Number = episode.Number,
+                            Season = episode.Season.Number,
+                            SeriesName = episode.Season.Series.Name,
+                            SeriesSlug = episode.Season.Series.Slug
+                        }
+                    }
+                };
+
+                return View(model);
+
+            }
+
+            else if (type == "movie")
+            {
+                var movie = await _movieRepository.GetMovieById(id);
+
+                if (movie == null)
+                {
+                    Response.StatusCode = 404;
+                    ViewBag.ErrorMessage = $"Movie with Id = {id} cannot be found";
+                    return View("NotFound");
+                }
+
+                var model = new CreateTimestampViewModel
+                {
+                    Content = new ContentDetailsViewModel
+                    {
+                        Movie = new RelatedMovieViewModel
+                        {
+                            Name = movie.Name,
+                            Slug = movie.Slug
+                        }
+                    }
+                };
+
+                return View(model);
+            }
+
+            Response.StatusCode = 404;
+            ViewBag.ErrorMessage = $"{type} cannot be found";
+            return View("NotFound");
         }
     }
 }
