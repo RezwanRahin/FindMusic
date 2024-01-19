4d3658da26bbc28bfd3b4d5c177b1ef64e875425
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-19 16:56:16 +0600

    added a property of 'TimestampDetailsViewModel' to 'EpisodeDetailsViewModel' class

--- FindMusic/ViewModels/EpisodeViewModels/EpisodeDetailsViewModel.cs
+++ FindMusic/ViewModels/EpisodeViewModels/EpisodeDetailsViewModel.cs
@@ -1,7 +1,9 @@
+using FindMusic.ViewModels.TimestampViewModels;
+
 namespace FindMusic.ViewModels.EpisodeViewModels
 {
     public class EpisodeDetailsViewModel : EpisodeContentViewModel
     {
-
+        public ICollection<TimestampDetailsViewModel>? Timestamps { get; set; }
     }
 }
