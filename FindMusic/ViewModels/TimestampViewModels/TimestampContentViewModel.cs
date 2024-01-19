5d5e636e838e8381fdfa71f9b07dec17740f79d0
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-19 16:44:01 +0600

    created a ViewModel named 'TimestampContentViewModel'

---
+++ FindMusic/ViewModels/TimestampViewModels/TimestampContentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMusic.ViewModels.TimestampViewModels
+{
+    public class TimestampContentViewModel
+    {
+        public virtual int Hour { get; set; }
+        public virtual int Minute { get; set; }
+        public virtual int Second { get; set; }
+
+        public ContributorViewModel Contributor { get; set; }
+
+    }
+}
