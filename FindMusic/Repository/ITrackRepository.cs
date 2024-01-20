a2245686c6309d5a8360a5861190307178b985eb
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-20 16:30:38 +0600

    created a repository interface named 'ITrackRepository'

---
+++ FindMusic/Repository/ITrackRepository.cs
@@ -0,0 +1,15 @@
+﻿using FindMusic.Models;
+
+namespace FindMusic.Repository
+{
+    public interface ITrackRepository
+    {
+        Task<Track> Add(Track track);
+        Task<Track?> GetTrack(int id);
+        Task<Track?> GetTrackWithRelatedData(int id);
+        Task<Track?> GetTrackWithEpisodeData(int id);
+        Task<Track?> GetTrackWithMovieData(int id);
+        Task<Track> Update(Track modifiedTrack);
+        Task<Track> Delete(Track track);
+    }
+}
