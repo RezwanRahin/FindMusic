1556028899764a5826773b39db05786bcdee2366
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-20 16:33:01 +0600

    created a repository class, implementing its interface, named 'TrackRepository'

---
+++ FindMusic/Repository/TrackRepository.cs
@@ -0,0 +1,42 @@
+﻿using FindMusic.Models;
+
+namespace FindMusic.Repository
+{
+    public class TrackRepository : ITrackRepository
+    {
+        public Task<Track> Add(Track track)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track> Delete(Track track)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track?> GetTrack(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track?> GetTrackWithEpisodeData(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track?> GetTrackWithMovieData(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track?> GetTrackWithRelatedData(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track> Update(Track modifiedTrack)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
