20ac3f5c27b3cf39d102e07ea65a59cc8394f496
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-20 16:35:15 +0600

    added a constructor, injecting 'AppDbContext', to 'TrackRepository'

--- FindMusic/Repository/TrackRepository.cs
+++ FindMusic/Repository/TrackRepository.cs
@@ -1,9 +1,17 @@
-﻿using FindMusic.Models;
+﻿using FindMusic.Context;
+using FindMusic.Models;
 
 namespace FindMusic.Repository
 {
     public class TrackRepository : ITrackRepository
     {
+        private readonly AppDbContext _context;
+
+        public TrackRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public Task<Track> Add(Track track)
         {
             throw new NotImplementedException();
