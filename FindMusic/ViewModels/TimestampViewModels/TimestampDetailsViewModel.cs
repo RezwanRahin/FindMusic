8a9d814b0cf385d3c212ad963488ffc97791e484
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-20 17:03:27 +0600

    added a property of 'TrackDetailsViewModel' to 'TimestampDetailsViewModel' class

--- FindMusic/ViewModels/TimestampViewModels/TimestampDetailsViewModel.cs
+++ FindMusic/ViewModels/TimestampViewModels/TimestampDetailsViewModel.cs
@@ -1,7 +1,10 @@
+using FindMusic.ViewModels.TrackViewModels;
+
 namespace FindMusic.ViewModels.TimestampViewModels
 {
     public class TimestampDetailsViewModel : TimestampContentViewModel
     {
         public int Id { get; set; }
+        public ICollection<TrackDetailsViewModel>? Tracks { get; set; }
     }
 }
