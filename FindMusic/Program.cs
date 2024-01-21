3534a8fc9179a19611e2624ae59aa72ded7bbd45
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-21 15:50:38 +0600

    registered 'TrackRepository' service in project Program file

--- FindMusic/Program.cs
+++ FindMusic/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddScoped<ISeasonRepository, SeasonRepository>();
 builder.Services.AddScoped<IEpisodeRepository, EpisodeRepository>();
 builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 builder.Services.AddScoped<ITimestampRepository, TimestampRepository>();
+builder.Services.AddScoped<ITrackRepository, TrackRepository>();
 
 // Add authorization options
 builder.Services.AddAuthorization(options =>
