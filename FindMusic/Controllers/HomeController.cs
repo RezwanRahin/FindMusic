92fa3b6433d703b8fef977e59505183e6d020e6e
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-21 16:38:30 +0600

    added 'AllowAnonymous' attribute to 'HomeController' class

--- FindMusic/Controllers/HomeController.cs
+++ FindMusic/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FindMusic.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FindMusic.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
