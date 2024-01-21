6499d74e511883426fc9bae2c42d4064e98cecd3
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-21 11:09:17 +0600

    added a HttpGet action method named 'Create' to 'TrackController' class

--- FindMusic/Controllers/TrackController.cs
+++ FindMusic/Controllers/TrackController.cs
@@ -1,5 +1,7 @@
 ﻿using FindMusic.Models;
 using FindMusic.Repository;
+using FindMusic.ViewModels;
+using FindMusic.ViewModels.TrackViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +19,64 @@ namespace FindMusic.Controllers
             _userManager = userManager;
             _trackRepository = trackRepository;
             _timestampRepository = timestampRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create(int timestampId)
+        {
+            var timestamp = await _timestampRepository.GetTimestampWithRelatedData(timestampId);
+
+            if (timestamp == null)
+            {
+                Response.StatusCode = 404;
+                ViewBag.ErrorMessage = $"Timestamp of Id = {timestampId} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new CreateTrackViewModel
+            {
+                Timestamp = new RelatedTimestampViewModel
+                {
+                    Id = timestamp.Id,
+                    Hour = timestamp.Hour,
+                    Minute = timestamp.Minute,
+                    Second = timestamp.Second
+                }
+            };
+
+            if (timestamp.Episode != null)
+            {
+                model.Content = new ContentDetailsViewModel
+                {
+                    Episode = new RelatedEpisodeViewModel
+                    {
+                        Number = timestamp.Episode.Number,
+                        Season = timestamp.Episode.Season.Number,
+                        SeriesName = timestamp.Episode.Season.Series.Name,
+                        SeriesSlug = timestamp.Episode.Season.Series.Slug
+                    }
+                };
+
+                return View(model);
+            }
+
+            else if (timestamp.Movie != null)
+            {
+                model.Content = new ContentDetailsViewModel
+                {
+                    Movie = new RelatedMovieViewModel
+                    {
+                        Name = timestamp.Movie.Name,
+                        Slug = timestamp.Movie.Slug
+                    }
+                };
+
+                return View(model);
+            }
+
+            Response.StatusCode = 404;
+            ViewBag.ErrorMessage = "Issue with content";
+            return View("NotFound");
         }
     }
 }
