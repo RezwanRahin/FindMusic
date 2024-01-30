c004a3cf7a61a50a845619409c9ba847a085bf72
rezwan <rezwanshahriorrahin@gmail.com>
2024-01-30 20:11:40 +0600

    added a constructor, injecting 'UserManager', 'ITrackRepository', and 'ITimestampRepository' services, to 'TrackController'

--- FindMusic/Controllers/TrackController.cs
+++ FindMusic/Controllers/TrackController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FindMusic.Models;
+using FindMusic.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FindMusic.Controllers
 {
     public class TrackController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ITrackRepository _trackRepository;
+        private readonly ITimestampRepository _timestampRepository;
 
+        public TrackController(UserManager<ApplicationUser> userManager, ITrackRepository trackRepository,
+            ITimestampRepository timestampRepository)
+        {
+            _userManager = userManager;
+            _trackRepository = trackRepository;
+            _timestampRepository = timestampRepository;
+        }
     }
 }
